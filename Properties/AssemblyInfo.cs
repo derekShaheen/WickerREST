b064f68623bab368380395554753985abe0e4c95
dm-develop <dm_dev@mailbox.org>
2024-03-28 16:10:26 +0100

    updated version info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using dm.ffmods.combattweaks;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(CombatTweaksMelon), "CombatTweaks", "1.0.0", "DM")]
+[assembly: MelonInfo(typeof(CombatTweaksMelon), "CombatTweaks", "1.0.1", "DM")]
 [assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
