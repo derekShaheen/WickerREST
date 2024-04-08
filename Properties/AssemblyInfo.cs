6747afa24f0962ebc87e6c51124bfbcd12d968c2
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2024-04-08 01:40:20 -0500

    Updating version, missed assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(WickerREST.WickerServer), "WickerREST", "0.95.1", "Skrip")]
+[assembly: MelonInfo(typeof(WickerREST.WickerServer), "WickerREST", "0.95.2", "Skrip")]
 [assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
 [assembly: MelonColor(ConsoleColor.Magenta)]
