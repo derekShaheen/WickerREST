004358f2f42a05295f60bf25b8887848d552aae4
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2024-04-08 18:22:57 -0500

    Removed redundent message

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: MelonInfo(typeof(WickerREST.WickerServer), "WickerREST", "0.95.2", "Skrip")]
 [assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
-[assembly: MelonColor(ConsoleColor.Magenta)]
+[assembly: MelonColor(255, 0, 255, 0)]
